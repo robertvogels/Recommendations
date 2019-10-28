76022809dbd8a7623b798af0d3fb6ebc1414f428
Robert Vogels <robertvogels@gmx.com>
2019-10-28 03:02:42 +0100

    Initial Commit

---
+++ Recommendations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Recommendations
+//
+//  Created by Robert on 28/10/2019.
+//  Copyright © 2019 Robert Vogels. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
